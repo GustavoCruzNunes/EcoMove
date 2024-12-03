8f99348e94baf1f13169b5e31f8ef9957df36ce2
Gustavo Nunes <Gustavocnunes13@gmail.com>
2024-12-02 22:07:31 -0300

    Manifest

--- app/src/main/java/com/example/pi5_ecomove/EditActivity.kt
+++ app/src/main/java/com/example/pi5_ecomove/EditActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
-class EditTripActivity : AppCompatActivity() {
+class EditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
